7468e2c1f1b3b4a56fb8391920abc9b42cf82145
yyonce <163499910+yyonce@users.noreply.github.com>
2025-04-04 19:01:32 +0800

    Create Application.java

---
+++ yyonce/downloader/Application.java
@@ -0,0 +1,9 @@
+// YYOnce Open Source Project
+
+// This version is an alpha version and is for testing purposes only
+
+package yyonce.downloader;
+
+public final class Application extends android.app.Application {
+
+}
